322868fcd4cb5cfc403f5fdca3e8842e68b1f1b2
manikanta2901 <manikanta2901@gmail.com>
2021-10-29 16:32:03 +0530

    Updated Changes

--- DataStructures/Stack/NextImmediateSmallerNumber.cpp
+++ DataStructures/Stack/NextImmediateSmallerNumber.cpp
@@ -1,26 +1,44 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int main(){
-    int size;
+
+  	
+int main() {
+	int size;
     cin >> size;
-    vector<int> nums;
+    vector<int>nums;
     for(int i = 0; i < size; i++){
         int a;
         cin >> a;
         nums.push_back(a);
     }
-
-    stack<int>answer;
-    for(int i = 0; i < size; i++){
-        while(!answer.empty() && answer.top() >= nums[i]){
-            answer.pop();
+  	stack <int> s;
+  	vector<int> ans;
+  	for(int i = size-1; i >= 0; i--){
+    	if(s.empty()){
+          	s.push(nums[i]);
+        	ans.push_back(nums[i]);
+        }else if(s.top() < nums[i]){
+            ans.push_back(s.top());
+            s.push(nums[i]);
+        }else{
+        	while(!s.empty()){
+            	if(s.top() < nums[i]){
+                  	ans.push_back(s.top());
+            		s.push(nums[i]);
+                  	break;
+                }
+              	s.pop();
+            }
+          	if(s.empty()){
+              	s.push(nums[i]);
+            	ans.push_back(nums[i]);
+            }
         }
-        if(!answer.empty()){
-            cout << answer.top() << " ";
-        }
-        answer.push(nums[i]);
     }
-    cout << nums[size - 1] << endl;
+  	reverse(ans.begin(), ans.end());
+  	nums = ans;
+  	for(auto num:nums)
+       cout<<num<<" ";
     return 0;
 }
